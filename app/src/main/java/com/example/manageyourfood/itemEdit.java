b7ba1594b10eabd05c44cb29a38ea3984e628765
jespy94 <espyboys@gmail.com>
2020-02-14 08:28:11 -0500

    Initial commit

---
+++ app/src/main/java/com/example/manageyourfood/itemEdit.java
@@ -0,0 +1,14 @@
+package com.example.manageyourfood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class itemEdit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_edit);
+    }
+}
